767f5f169aa42934e3f9cee6b2c3a2407dd1244f
Tedy Fernando <teddyfernando45@gmail.com>
2024-10-06 21:00:34 +0700

    Update Docker Add Postgre and Wait Db Test

--- app/app/test.py
+++ app/app/test.py
@@ -1,10 +1,10 @@
 # sample test
-
 from django.test import SimpleTestCase
-
 from app import calc
 
+
 class CalcTest(SimpleTestCase):
+
     def test_add_number(self):
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
