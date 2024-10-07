9893fd436c5a961c6ae8ff1fa263da385e5be301
Tedy Fernando <teddyfernando45@gmail.com>
2024-10-07 23:03:56 +0700

    Create Super User Test & Model

--- app/core/models.py
+++ app/core/models.py
@@ -9,11 +9,20 @@ from django.contrib.auth.models import (
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError("user must have email address!")
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
