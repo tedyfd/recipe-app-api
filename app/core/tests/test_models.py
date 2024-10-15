1d6a23b5077562421f6fb28a978c702897ec498b
Tedy Fernando <teddyfernando45@gmail.com>
2024-10-15 22:19:48 +0700

    Add Recipe Tag

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,6 +6,10 @@ from django.contrib.auth import get_user_model
 from core import models
 
 
+def create_user(email='user@example.com', password='pass12345'):
+    return get_user_model().objects.create_user(email, password)
+
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email_sucessful(self):
@@ -57,3 +61,9 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
